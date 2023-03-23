cc149ed78eb74f24eff287596b25f1d4658bfe82
mahmuth <mgol@protel.com.tr>
2023-03-23 18:22:33 +0300

    [Feat] Add a new locale object to all components

---
+++ src/commands/addNewLocale.js
@@ -0,0 +1,43 @@
+import yargs from 'yargs'
+import { updateComponent } from '../utils/index.js'
+
+function addNewLocale(tagContent, newKey, extendKey = '') {
+  const updatedContent = { ...tagContent }
+  updatedContent[newKey] = {}
+
+  if (extendKey) {
+    updatedContent[newKey] = updatedContent[extendKey]
+  }
+
+  return updatedContent
+}
+
+yargs.command({
+  command: 'add-locale',
+  describe: 'Add new locale to all components',
+  builder: {
+    localeKey: {
+      alias: 'l',
+      describe: 'New locale key.',
+      demandOption: true
+    },
+    extendKey: {
+      alias: 'e',
+      describe: 'Locale key to extend.'
+    },
+    dir: {
+      alias: 'd',
+      describe: 'Components directory to add new locale.'
+    }
+  },
+  handler: (argv) => {
+    const { localeKey, extendKey, dir } = argv
+
+    const updatedContent = (content) =>
+      addNewLocale(content, localeKey, extendKey)
+    updateComponent(updatedContent, dir)
+  }
+})
+
+yargs.help()
+yargs.parse()
